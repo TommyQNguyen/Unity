7b75b0a34bb7edf98cdace3bda3a24a83227de0d
Tommy <e.qtnguyen@etu.cvm.qc.ca>
2021-02-01 17:12:16 -0500

    First commit

---
+++ Assets/ControllerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+
+    public float Speed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        var direction = new Vector3(horizontal, vertical, 0).normalized;
+
+        if (direction.magnitude > 1)
+        {
+            direction = direction.normalized;
+        }
+
+
+        transform.position += new Vector3(horizontal * Speed * Time.deltaTime, vertical * Speed * Time.deltaTime, 0);
+
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float Speed;
+
+    private float _secret;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Quaternion
+        // Euler angles
+        // delta = 0.02
+        transform.rotation *= Quaternion.Euler(0, Speed * Time.deltaTime, 0);
+
+    
+    }
+}
