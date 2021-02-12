e3a7888fa994ee67664ffd6014075107a13dc8f0
Tommy <e.qtnguyen@etu.cvm.qc.ca>
2021-02-12 16:23:31 -0500

    My game has an exploding barrel yay!

--- Assets/Barrel.cs
+++ Assets/Barrel.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-
+    public float BarrelTimer = 2;
+    public GameObject Bullet; // Drag my Explosion prefab in Unity
+    public GameObject Explosion; // Drag my Explosion prefab in Unity
     public Health Health { get; private set; }
     public Flash Flash { get; private set; }
+    //private bool isDetonating = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,25 +21,75 @@ public class Barrel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Flash.enabled)
+        {
+            BarrelTimer = BarrelTimer - Time.deltaTime;
+            if (BarrelTimer < 0)
+            {
+                Debug.Log("Barrel Timer: " + BarrelTimer);
+                Destroy(gameObject);
+                Instantiate(Explosion, transform.position, Quaternion.identity);
+
+                // A le rendre plus optimal quand j'aurai fini les autres taches... 
+                var bulletRotation1 = transform.rotation * Quaternion.Euler(0, 0, 0);       // Se dirige tout droit
+                var bulletRotation2 = transform.rotation * Quaternion.Euler(45, 0, -45);  // Se dirige en diagonale en haut
+                var bulletRotation3 = transform.rotation * Quaternion.Euler(90, 0, -90);    // Se dirige en diagonale en bas
+                var bulletRotation4 = transform.rotation * Quaternion.Euler(135, 0, -135);       // Se dirige tout droit
+                var bulletRotation5 = transform.rotation * Quaternion.Euler(180, 0, -180);  // Se dirige en diagonale en haut
+                var bulletRotation6 = transform.rotation * Quaternion.Euler(225, 0, -225);    // Se dirige en diagonale en bas
+                var bulletRotation7 = transform.rotation * Quaternion.Euler(270, 0, -270);       // Se dirige tout droit
+                var bulletRotation8 = transform.rotation * Quaternion.Euler(315, 0, -315);  // Se dirige en diagonale en haut
+
+                // Shoot les bullets partout sur explosion
+                Instantiate(Bullet, transform.position, bulletRotation1);
+                Instantiate(Bullet, transform.position, bulletRotation2);
+                Instantiate(Bullet, transform.position, bulletRotation3);
+                Instantiate(Bullet, transform.position, bulletRotation4);
+                Instantiate(Bullet, transform.position, bulletRotation5);
+                Instantiate(Bullet, transform.position, bulletRotation6);
+                Instantiate(Bullet, transform.position, bulletRotation7);
+                Instantiate(Bullet, transform.position, bulletRotation8);
+            }
+        }
     }
 
     public void onBulletCollision(Bullet bullet)
     {
         // Should only the player have a health component? because monster and spawner and barrels are made in more than 1 unlike player
-        //Health.BarrelHealthQuantity = Health.BarrelHealthQuantity - 1;
+        Health.BarrelHealthQuantity = Health.BarrelHealthQuantity - 1;
 
-        //if (Health.BarrelHealthQuantity > 0)
-        //{
-        //    Flash.StartFlash();
-        //}
 
-        //if (Health.BarrelHealthQuantity < 1)
-        //{
-        //    Debug.Log("Enemy is dead");
-        //    Destroy(gameObject);
-        //}
+        if (Health.BarrelHealthQuantity > 0)
+        {
+            Flash.StartFlash();
+        }
+        else if (Health.BarrelHealthQuantity < 1)
+        {
+            Debug.Log("Barrel is dead");
+            Destroy(gameObject);
+            Instantiate(Explosion, transform.position, Quaternion.identity);
+
+            var bulletRotation1 = transform.rotation * Quaternion.Euler(0, 0, 0);       // Se dirige tout droit
+            var bulletRotation2 = transform.rotation * Quaternion.Euler(45, 0, -45);  // Se dirige en diagonale en haut
+            var bulletRotation3 = transform.rotation * Quaternion.Euler(90, 0, -90);    // Se dirige en diagonale en bas
+            var bulletRotation4 = transform.rotation * Quaternion.Euler(135, 0, -135);       // Se dirige tout droit
+            var bulletRotation5 = transform.rotation * Quaternion.Euler(180, 0, -180);  // Se dirige en diagonale en haut
+            var bulletRotation6 = transform.rotation * Quaternion.Euler(225, 0, -225);    // Se dirige en diagonale en bas
+            var bulletRotation7 = transform.rotation * Quaternion.Euler(270, 0, -270);       // Se dirige tout droit
+            var bulletRotation8 = transform.rotation * Quaternion.Euler(315, 0, -315);  // Se dirige en diagonale en haut
+
+            // Shoot les bullets partout sur explosion
+            Instantiate(Bullet, transform.position, bulletRotation1);
+            Instantiate(Bullet, transform.position, bulletRotation2);
+            Instantiate(Bullet, transform.position, bulletRotation3);
+            Instantiate(Bullet, transform.position, bulletRotation4);
+            Instantiate(Bullet, transform.position, bulletRotation5);
+            Instantiate(Bullet, transform.position, bulletRotation6);
+            Instantiate(Bullet, transform.position, bulletRotation7);
+            Instantiate(Bullet, transform.position, bulletRotation8);
+        }
 
         Debug.Log("Barrel is hit by Bullet");
+        Debug.Log("Barrel Health left: " + Health.BarrelHealthQuantity);
     }
 }
