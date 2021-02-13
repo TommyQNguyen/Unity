0815cf3ee3f7b17350bda7b8f720eb1e1e42c742
Tommy <e.qtnguyen@etu.cvm.qc.ca>
2021-02-13 17:42:10 -0500

    Perdu ma Scene et hierachy... a recommence...

--- Assets/PlayerHud.cs
+++ Assets/PlayerHud.cs
@@ -7,11 +7,19 @@ public class PlayerHud : MonoBehaviour
 {
     public Text PlayerHealthText;
     public Image PlayerHealthImage;
+    public Health Health { get; private set; }
+
+    public void Awake()
+    {
+        Health = GetComponent<Health>(); 
+    }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerHealthText.text = "Bonjour!";
+        // a voir pourquoi les points de vies ne s'affichent pas
+        //PlayerHealthText.text = "x " + Health.PlayerHealthQuantity;
+        PlayerHealthText.text = "Bonjour";
         PlayerHealthText.color = PlayerHealthText.color.WithAlpha(0.5f);
         PlayerHealthImage.color = PlayerHealthText.color.WithAlpha(0.5f);
     }
