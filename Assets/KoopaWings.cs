3e8837c15528786bea373527882b09679544ecc1
Tommy <e.qtnguyen@etu.cvm.qc.ca>
2021-03-01 09:30:53 -0500

    Koopa almost done. Add Shell and fix Koopa Health

--- Assets/KoopaWings.cs
+++ Assets/KoopaWings.cs
@@ -7,29 +7,75 @@ public class KoopaWings : MonoBehaviour
 {
     public Vector2 RunAnimationSpeed;
     public PlatformController PlatformController { get; private set; }
+    public Health Health { get; private set; }
     public Animator Animator { get; private set; }
 
     // Start is called before the first frame update
     void Awake()
     {
         PlatformController = GetComponent<PlatformController>();
-        PlatformController.OnMoveStart += OnMoveStart;
+        PlatformController.OnJump += OnJump;
+        PlatformController.OnLand += OnLand;
+
+        Health = GetComponent<Health>();
+        //Health.OnDeath += OnDeath;
+        Health.OnHit += OnHit;
+        Health.OnDeath += OnDeath;
 
         Animator = GetComponent<Animator>();
     }
 
-    private void OnMoveStart(PlatformController platformController)
+    private void OnLand(PlatformController platformController)
+    {
+        Animator.Play("Koopa_Walk");
+    }
+
+    private void OnJump(PlatformController platformController)
     {
         Animator.Play("Koopa_Fly");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnHit(Health health)
+    {
+        Health.Value -= 1;
+        //Animator.Play("Koopa_Walk");
+    }
+
+    private void OnDeath(Health health)
+    {
+        Destroy(gameObject);
+
+        // Pour spawner un Shell a la mort du Koopa
+        //GameManager.Instance.PrefabManager.PlatformerSpawn(PrefabManager.PlatformerGlobal.Goomba, transform.position);
+    }
+
+    private void Koopa_Fly()
     {
         PlatformController.InputMove = PlatformController.FacingController.Direction;
         PlatformController.InputJump = true;
 
         var speedRatio = PlatformController.CurrentSpeed / PlatformController.MoveSpeed;
         Animator.speed = RunAnimationSpeed.Lerp(speedRatio);
+    }
+
+    private void Koopa_Walk()
+    {
+        PlatformController.InputMove = PlatformController.FacingController.Direction;
+
+        var speedRatio = PlatformController.CurrentSpeed / PlatformController.MoveSpeed;
+        Animator.speed = RunAnimationSpeed.Lerp(speedRatio);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Health.Value >= 2)
+        {
+            Koopa_Fly();
+        }
+        else if (Health.Value < 2)
+        {
+            Koopa_Walk();
+        }
     }
 }
