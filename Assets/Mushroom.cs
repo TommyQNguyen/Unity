492e3688ed297bc6e0268e97f5489914863bf6ad
Tommy <e.qtnguyen@etu.cvm.qc.ca>
2021-03-03 13:18:43 -0500

    Start of the Mushroom

---
+++ Assets/Mushroom.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mushroom : MonoBehaviour
+{
+    public PlatformController PlatformController { get; private set; }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PlatformController = GetComponent<PlatformController>();
+        PlatformController.OnWall += OnWall;
+    }
+
+    private void OnWall(PlatformController platformController)
+    {
+        PlatformController.FacingController.Flip();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlatformController.InputMove = PlatformController.FacingController.Direction;
+    }
+
+    
+}
